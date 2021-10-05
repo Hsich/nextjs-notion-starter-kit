66419a92612d4d7f29960f5fbb3e003cadcefa5c
Hsich <39936164+Hsich@users.noreply.github.com>
2021-10-05 14:55:55 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Home-e47ba19f41354094904d9562c5f63318',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Goldtips',
+  domain: 'https://statuesque-barracuda-ffb.notion.site/Home-e47ba19f41354094904d9562c5f63318',
+  author: '厚金路',
 
   // open graph metadata (optional)
   description: 'Example site description',
