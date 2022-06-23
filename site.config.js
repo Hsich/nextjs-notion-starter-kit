b700c227364047a8d3cc9d82806273a6014d3837
Hsich <39936164+Hsich@users.noreply.github.com>
2022-06-23 21:39:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-//   rootNotionPageId: '111-9b2c0d7817db4766851e329e60b17d47',
   rootNotionPageId: 'c340fd6af44d4e98a3c049f35b0650ea',
 
   // if you want to restrict pages to a single notion workspace (optional)
@@ -10,7 +9,6 @@ module.exports = {
   // basic site info (required)
   name: 'Goldtips',
   domain: 'https://goldtips.notion.site/c340fd6af44d4e98a3c049f35b0650ea',
-//   domain: 'https://thickgold.notion.site/111-9b2c0d7817db4766851e329e60b17d47',
   author: '厚金路',
 
   // open graph metadata (optional)
